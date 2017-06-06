c9c2e6f74311adca66fb7b0fb8e454e8994b62e0
msx752 <aslim@outlook.com.tr>
2017-06-06 21:22:07 +0300

    tryng to implementation

---
+++ CoreBot.RocketAPI/WebProxy.cs
@@ -0,0 +1,24 @@
+﻿#region using directives
+
+using System;
+using System.Net;
+
+#endregion using directives
+
+namespace CoreBot.RocketAPI
+{
+    public class WebProxy : IWebProxy
+    {
+        public Uri GetProxy(Uri destination)
+        {
+            return new Uri("http://localhost:8888");
+        }
+
+        public bool IsBypassed(Uri host)
+        {
+            return false;
+        }
+
+        public ICredentials Credentials { get; set; }
+    }
+}
